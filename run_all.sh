369e111647ea6dfd52e0323498f63af5b46eb237
AlexandrosAlexiou <cs02929@uoi.gr>
2020-07-01 20:51:17 +0300

    Improve bash script that runs all tests

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-# run all test files
-./mppc.py tests/test1.min
-./mppc.py tests/test2.min
-./mppc.py tests/test3.min
-./mppc.py tests/test4.min
-./mppc.py tests/test5.min
-./mppc.py tests/test6.min
-./mppc.py tests/test7.min
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+for i in {1..7}
+do
+  echo "${RED}Running test${i}.min${NC}"
+  ./mppc.py tests/test$i.min
+  printf '\n\n'
+done
