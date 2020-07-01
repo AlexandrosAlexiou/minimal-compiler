0b85da8499a8cde25d44915481f8d748c69954b2
Alexandros Alexiou <AlexandrosAlexiou@users.noreply.github.com>
2020-07-01 22:26:27 +0300

    Add user confirmation to delete generated files

--- clean.sh
+++ clean.sh
@@ -1,8 +1,16 @@
-#!/usr/bin/env bash
+# find generated files and delete them
+RED='\033[91m'
+GREEN='\033[92m'
+NC='\033[0m' # No Color
 
 # find generated files and delete them
-
+echo "${RED}This will delete mpcc generated file from ./tests folder. Are you sure?${NC} (${RED}Y${NC}/${GREEN}n${NC})"
+read -r input
+if [[  $input =~ ^[Y]$ ]]
+then
+# Do dangerous stuff
 find ./tests -name "*.int" -type f -delete
 find ./tests -name "*.c" -type f -delete
 find . -name "*.out" -type f -delete
 find ./tests -name "*.asm" -type f -delete
+fi
--- run_all.sh
+++ run_all.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-
-RED='\033[0;31m'
+RED='\033[91m'
 NC='\033[0m' # No Color
 
 for i in {1..7}
 do
-  echo "${RED}Running test${i}.min${NC}"
+  echo "${RED} Running test${i}.min ${NC}"
   ./mppc.py tests/test$i.min
   printf '\n\n'
 done
